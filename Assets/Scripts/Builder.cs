78b54ff3badec3a7470af77751dac35bf6dfc397
mistueh <58933430+Michael-Stuehle@users.noreply.github.com>
2020-08-20 12:34:51 +0200

    F5 / F3 tasten standart funktionen im browser abgesschaltet

--- Assets/Scripts/Builder.cs
+++ Assets/Scripts/Builder.cs
@@ -7,11 +7,13 @@ public class Builder
 {
     public static void BuildWebGl()
     {
+#if UNITY_EDITOR
         BuildPipeline.BuildPlayer(
              new[] { "Assets/Scenes/SampleScene.unity", "Assets/Scenes/Death_Scene.unity" },
              "Build/WebGl/",
              BuildTarget.WebGL,
              BuildOptions.None);
+#endif
     }
 }
 
