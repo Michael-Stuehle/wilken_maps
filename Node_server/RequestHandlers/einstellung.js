025ec0759ca2ed9464f7e4982a267261bf4c9fc4
mistueh <58933430+Michael-Stuehle@users.noreply.github.com>
2021-01-18 08:59:21 +0100

    Node server: einstellung seite code lesbarer gemacht

--- Node_server/RequestHandlers/einstellung.js
+++ Node_server/RequestHandlers/einstellung.js
@@ -61,54 +61,55 @@ module.exports = {
 
 var buildClientJS = function(){
     let retVal =
-     '<script src="/script.js"></script>'+linebreak+
-     '<script>'+linebreak+
-        'function DoSubmit(){'+linebreak+
-            'document.getElementById("form").submit();'+linebreak+
-        '}'+linebreak+
-        linebreak+
-        'document.addEventListener("keydown", function(event){'+linebreak+
-            'if (event.ctrlKey && event.key.toLocaleLowerCase() == "s") {'+linebreak+
-                'event.preventDefault();'+linebreak+
-                'DoSubmit();'+linebreak+
-            '}'+linebreak+
-        '}); '+linebreak+
-        linebreak+
-        
-        'function saveResult(){'+linebreak+
-            'setTimeout(function () {'+linebreak+
-                'var iframe = document.getElementById("resultFrame");'+linebreak+
-                'var resultText = iframe.contentWindow.document.body.innerHTML;'+linebreak+
-                ' if (resultText === "") {'+linebreak+
-                  // do nothing
-                '} else {'+linebreak+
-                    'location.reload();' +linebreak+
-                    'setTimeout(function(){ window.alert(resultText); }, 100);'+linebreak+
-                '}'+linebreak+
-            '}, 10);'+linebreak+
-        '}'+linebreak+
-    '</script>';
+    `<script src="/script.js"></script>
+    <script>
+       function DoSubmit(){
+           document.getElementById("form").submit();
+       }
+
+       document.addEventListener("keydown", function(event){
+           if (event.ctrlKey && event.key.toLocaleLowerCase() == "s") {
+               event.preventDefault();
+               DoSubmit();
+           }
+       }); 
+       
+       function saveResult(){
+           setTimeout(function () {
+               var iframe = document.getElementById("resultFrame");
+               var resultText = iframe.contentWindow.document.body.innerHTML;
+                if (resultText === "") {
+                 // do nothing
+               } else {
+                   location.reload(); 
+                   setTimeout(function(){ window.alert(resultText); }, 100);
+               }
+           }, 10);
+        }
+    </script>`;
     return retVal;
 }
 
 var buildSubmitBtn = function(){
-    let retVal = '<input type="submit" id="btn-submit" value="Speichern" onclick="DoSubmit()"/>'+linebreak;
+    let retVal = `<input type="submit" id="btn-submit" value="Speichern" onclick="DoSubmit()"/>`+linebreak;
     return retVal;
 }
 
 var buildRaumEinstellung = function(element){
-    var retVal = '<div class="item select" autofocus>'+linebreak+
-    '<label class="selectLabel">Raum: </label>'+linebreak+
-    '<select id="raum-select" name="raum">'+linebreak;
+    var retVal = 
+    `<div class="item select" autofocus>
+    <label class="selectLabel">Raum: </label>
+    <select id="raum-select" name="raum">`+linebreak;
     for (let index = 0; index < element.options.length; index++) {
         const opt = element.options[index];
         if (element.value == opt.id) {
-            retVal += '<option value=' + opt.id + ' selected>'+opt.value+'</option>'+linebreak;
+            retVal += `<option value="${opt.id}" selected>${opt.value}</option>`+linebreak;
         }else{
-            retVal += '<option value=' + opt.id + '>'+opt.value+'</option>'+linebreak;
+            retVal += `<option value="${opt.id}">${opt.value}</option>`+linebreak;
         }
     }
-    retVal += '</select>' + '</div>'+linebreak;;
+    retVal += `</select>
+            </div>`+linebreak;
     return retVal;
 }
 
@@ -119,57 +120,58 @@ var buildBoolInput = function(element){
     }else{
         valStr = '';
     }
-    var result = '<div class="item">'+linebreak+
-    '<label for="edt'+element.name+'">'+element.name+'</label>' +linebreak+
-    '<label class="switch">' +linebreak+
-        '<input type="checkbox" name="'+element.name+'" id="edt'+element.name+'" '+valStr+linebreak+
-            ' onclick="DoSubmit()">' +linebreak+
-        '<span class="slider round"></span>' +linebreak+
-    '</label>' +linebreak+
-    '</div>';
+    var result = `<div class="item">
+    <label for="edt${element.name}">${element.name}</label>
+    <label class="switch">
+        <input type="checkbox" name="${element.name}" id="edt${element.name}" ${valStr} onclick="DoSubmit()">
+        <span class="slider round"></span>
+    </label>
+    </div>`;
 
     return result;
 }
 
 var buildIntInput = function(element){
-    let result = '<div class="item">'+linebreak+
-    '<label for="edt'+element.name+'">'+element.name+'</label>' +linebreak+
-    '<input type="number" name="'+element.name+'" id="edt'+element.name+'" value="'+element.value+'">' +linebreak+
-    '</div>';
+    let result = 
+    `<div class="item">
+        <label for="edt${element.name}">${element.name}</label>
+        <input type="number" name="${element.name}" id="edt${element.name}" value="${element.value}">
+    </div>`;
     return result;
 }
 
 var buildStrInput = function(element){
-    let result = '<div class="item">'+linebreak+
-    '<label for="edt'+element.name+'">'+element.name+'</label>' +linebreak+
-    '<input name="'+element.name+'" id="edt'+element.name+'" value="'+element.value+'">'+linebreak+
-    '</div>';
-
+    let result =     
+    `<div class="item">
+        <label for="edt${element.name}">${element.name}</label>
+        <input name="${element.name}" id="edt${element.name}" value="${element.value}">
+    </div>`;
     return result;
 }
 
 var bulidHeader = function(){
-    let result = '<html>'+linebreak+
-                    '<head>'+linebreak+
-                        '<meta name="viewport" charset="utf-8" content="width=device-width, initial-scale=1.0" />'+linebreak+
-                        '<link rel="stylesheet" href="einstellungen.css">'+linebreak+
-                        buildClientJS() +linebreak+
-                    '</head>'+linebreak+
-                    '<body>'+linebreak+
-                        '<h1>Einstellungen</h1> <br><br>'+linebreak+
-                        '<iframe id="resultFrame" name="formDestination"  style="visibility: hidden; width: 0px; height: 0px" onload="saveResult()"></iframe>'+linebreak+
-                        '<div style="display: block;overflow: auto;">'+linebreak+
-                            '<form id="form" class="form" action="einstellungen" method="POST" target="formDestination">';
+    let result = `<html>
+                    <head>
+                        <meta name="viewport" charset="utf-8" content="width=device-width, initial-scale=1.0"/>
+                        <link rel="stylesheet" href="einstellungen.css">
+                        ${buildClientJS()}
+                    </head>
+                    <body>
+                        <h1>Einstellungen</h1> 
+                        <br><br>
+                        <iframe id="resultFrame" name="formDestination" style="visibility: hidden; width: 0px; height: 0px" onload="saveResult()"></iframe>
+                        <div style="display: block;overflow: auto;">
+                            <form id="form" class="form" action="einstellungen" method="POST" target="formDestination">`;
     return result;
 }
 
 
 var buildFooter = function(){
     let result = 
-                '</form></div>'+linebreak+
-                '<br>'+linebreak+
-                '<a style="float: left;margin: 0 auto" href="/home">zurück</a>'+linebreak+
-            '</body>'+linebreak+
-        '</html>';
+                `</form></div>
+                <br>
+                <a style="float: left;margin: 0 auto" href="/home">zurück</a>
+            </body>
+        </html>`;
     return result;
 }
